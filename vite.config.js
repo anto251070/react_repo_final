bc1a4da6e80201c8bf3ff583f22cb89a9f0106a2
AGI <anto251070@gmail.com>
2026-05-05 11:03:46 -0400

    feat: deploy react 2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/YOUR_REPOSITORY_NAME",
+    base: "/react_repo_final",
   plugins: [react()],
  
 })
